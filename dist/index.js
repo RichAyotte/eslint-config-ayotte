ead268c10e87e9f07780f42601169e1b3e4daf4b
Richard Ayotte <rich@ayottesoftware.com>
2023-09-22 19:15:41 -0400

    chore: bump deps

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const combine_rules_1 = require("./combine_rules");
-const extensions_1 = require("./extensions");
-const get_best_practices_rules_1 = require("./get_best_practices_rules");
-const get_error_rules_1 = require("./get_error_rules");
-const get_es6_rules_1 = require("./get_es6_rules");
-const get_node_rules_1 = require("./get_node_rules");
-const get_promise_rules_1 = require("./get_promise_rules");
-const get_strict_mode_rules_1 = require("./get_strict_mode_rules");
-const get_style_rules_1 = require("./get_style_rules");
-const get_variable_rules_1 = require("./get_variable_rules");
+const combine_rules_1 = __importDefault(require("./combine_rules"));
+const extensions_1 = __importDefault(require("./extensions"));
+const get_best_practices_rules_1 = __importDefault(require("./get_best_practices_rules"));
+const get_error_rules_1 = __importDefault(require("./get_error_rules"));
+const get_es6_rules_1 = __importDefault(require("./get_es6_rules"));
+const get_node_rules_1 = __importDefault(require("./get_node_rules"));
+const get_promise_rules_1 = __importDefault(require("./get_promise_rules"));
+const get_strict_mode_rules_1 = __importDefault(require("./get_strict_mode_rules"));
+const get_style_rules_1 = __importDefault(require("./get_style_rules"));
+const get_variable_rules_1 = __importDefault(require("./get_variable_rules"));
 const config = {
     env: {
         es6: true,
